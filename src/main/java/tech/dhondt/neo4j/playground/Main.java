2dce038dfd1265c2d94ea71d7a258548d711d25b
Maarten Dhondt <maarten.dhondt@skynet.be>
2018-07-24 23:01:59 +0200

    test connection

--- src/main/java/tech/dhondt/neo4j/playground/Main.java
+++ src/main/java/tech/dhondt/neo4j/playground/Main.java
@@ -1,8 +1,43 @@
 package tech.dhondt.neo4j.playground;
 
-public class Main {
+import org.neo4j.driver.v1.*;
+
+import static org.neo4j.driver.v1.Values.parameters;
+
+public class Main implements AutoCloseable {
+
+    private final Driver neo4jDriver;
+
+    private Main() {
+        this.neo4jDriver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "Playground"));
+    }
 
     public static void main(String[] args) {
         System.out.println("My NEO4J Playground");
+        try (Main main = new Main()) {
+            main.testConnection();
+        }
+    }
+
+    private void testConnection() {
+        try (Session session = neo4jDriver.session()) {
+            String greeting = session.writeTransaction(tx -> {
+                StatementResult result = tx.run("CREATE (a:ConnectionTest) " +
+                                                        "SET a.message = $message " +
+                                                        "RETURN a.message + ', from node ' + id(a)",
+                                                parameters("message", "Hello World"));
+                String testConnection = result.single().get(0).asString();
+
+                tx.run("MATCH (n:ConnectionTest) OPTIONAL MATCH (n)-[r]-() DELETE n, r"); // delete all nodes with a given label and their relationships
+
+                return testConnection;
+            });
+            System.out.println(greeting);
+        }
+    }
+
+    @Override
+    public void close() {
+        neo4jDriver.close();
     }
 }
